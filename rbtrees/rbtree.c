dbe0a1c647560fe5c23fadc7ddf49b5db664274e
lizzz <ltitov@unsa.edu.pe>
2024-11-27 19:22:33 -0500

    Cree las funciones rightRotate y leftRotate

--- rbtrees/rbtree.c
+++ rbtrees/rbtree.c
@@ -6,7 +6,7 @@ RBNode* createRBNode(int key){
   RBNode* newNode = malloc(sizeof(RBNode));
   newNode->parent = NULL;
   newNode->left = NULL;
-  newNode->rigth = NULL;
+  newNode->right = NULL;
   newNode->key = key;
   newNode->color = RED;
   return newNode;
@@ -22,3 +22,74 @@ int isRootBlack(RBTree *t){
   if(!t->root) return 1;
   return t->root->color == BLACK;
 }
+
+void rbInsert(RBTree *t, RBNode* z){
+  RBNode* x = t->root;
+  RBNode* y = NULL;
+
+  while(x){
+    y = x;
+    if(z->key < x->key)
+      x = x->left;
+    else
+      x = x->right;
+  }
+
+  z->parent = y;
+
+  if(!y)
+    t->root = z;
+  else{
+    if(z->key < y->key)
+      y->left = z;
+    else
+      y->right = z;
+  }
+  //rbInsertFixUp(t, z);
+}
+
+void leftRotate(RBTree *t, RBNode* x){
+  RBNode* y = NULL;
+  
+  y = x->right;
+  x->right = y->left;
+  
+  if(y->left != t->root)
+    y->left->parent = x;
+  
+  y->parent = x->parent;
+  
+  if(x->parent == t->root)
+    t->root = y;
+  else if(x == x->parent->left)
+    x->parent->left = y;
+  else
+    x->parent->right = y;
+  
+  y->left = x;
+  x->parent = y;
+}
+
+void rightRotate(RBTree *t, RBNode* y){
+  RBNode* x = t->root;
+  
+  y = x->left;
+  x->left = y->right;
+
+  if(y->right != t->root)
+    y->right->parent = x;
+
+  y->parent = x->parent;
+
+  if(x->parent == t->root)
+    t->root = y;
+  else if(x == x->parent->right)
+    x->parent->right = y;
+  else
+    x->parent->left = y;
+
+  y->right = x;
+  x->parent = y;
+}
+
+
